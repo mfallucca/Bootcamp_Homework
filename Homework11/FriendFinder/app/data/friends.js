5e0138e89b3eaf508d26172f26eec0ec836f2b86
Matthew Fallucca <mfallucca@Ashleys-MacBook-Pro.local>
2017-12-21 15:25:18 -0600

    adding homework11

---
+++ Homework11/FriendFinder/app/data/friends.js
@@ -0,0 +1,108 @@
+var friendsArray = [
+{
+  "name":"BubbRubb",
+  "photo":"http://i0.kym-cdn.com/photos/images/facebook/000/001/362/bubb-rubb-wheaties.jpg",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ],
+  "total": 32
+},
+{
+  "name":"LilSis",
+  "photo":"http://i3.ytimg.com/vi/jpl_ey5cVnk/hqdefault.jpg",
+  "scores":[
+      2,
+      1,
+      2,
+      2,
+      1,
+      1,
+      2,
+      3,
+      2,
+      1
+    ],
+  "total": 17
+},
+{
+  "name":"Trump",
+  "photo":"https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/08/03/104630007-GettyImages-825587596-donald-trump.jpg",
+  "scores":[
+      5,
+      3,
+      4,
+      4,
+      5,
+      3,
+      2,
+      5,
+      4,
+      3
+    ],
+  "total": 38
+},
+{
+  "name":"Cheeseburger",
+  "photo":"https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Cheeseburger.jpg/1200px-Cheeseburger.jpg",
+  "scores":[
+      1,
+      1,
+      4,
+      4,
+      1,
+      1,
+      2,
+      1,
+      4,
+      1
+    ],
+  "total": 20
+},
+{
+  "name":"Bob Barker",
+  "photo":"https://www.biography.com/.image/t_share/MTE4MDAzNDEwNzQ5MTk1Nzkw/bob-barker-9542237-1-402.jpg",
+  "scores":[
+      2,
+      1,
+      4,
+      4,
+      2,
+      1,
+      2,
+      2,
+      4,
+      1
+    ],
+  "total": 23
+},
+{
+  "name":"Bob Saget",
+  "photo":"https://mediamass.net/jdd/public/documents/celebrities/4056.jpg",
+  "scores":[
+      5,
+      2,
+      4,
+      4,
+      5,
+      2,
+      2,
+      5,
+      4,
+      2
+    ],
+  "total": 35
+}
+];
+
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friendsArray;
